f20935cdf770c0351593629c17cad3a055a652f2
Ernesto Lanchares <e.lancha98@gmail.com>
2018-01-31 15:58:01 +0100

    CONTRASEÑA YAY

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index')
 
 var app = express();
 
@@ -14,16 +12,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'GraFME', isValid: true });
+});
+
+router.post('/', function(req, res, next) {
+    if (req.body.contra === 'gaussstyle') {
+        res.render('grafo');
+    } else {
+        res.render('index', { title: 'GraFME', isValid: false});
+    }
 });
 
 module.exports = router;
