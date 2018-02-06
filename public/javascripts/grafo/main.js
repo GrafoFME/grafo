f93fe015a363b57a933dbe731c53eb0cf44f3abe
Ernesto Lanchares <e.lancha98@gmail.com>
2018-02-06 23:06:53 +0100

    Añadidas cosas

--- public/javascripts/grafo/main.js
+++ public/javascripts/grafo/main.js
@@ -1,9 +1,20 @@
+let scale;
+let mode;
+// r - aristas recientes
+// v - vertices con más aristas
+// k - mapa de k's
+// e - mapa de edades
+
+
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
     background(155);
+    let c1 = color(255, 0, 0);
+    let c2 = color(0, 255, 0);
 }
 
 function windowResized() {
